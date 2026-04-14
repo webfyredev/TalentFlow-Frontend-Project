8607ec3d54eebef830fee2215816ccdaee90d87c
impphilwebs <imphilwebs@gmail.com>
2026-04-14 07:56:38 +0100

    Styled the message on the sign up page

--- src/pages/auth/signUp.jsx
+++ src/pages/auth/signUp.jsx
@@ -67,7 +67,11 @@ export default function SignUpPage() {
       setErrorMsg("");
 
       // ✅ OPTIONAL: redirect to login
-      navigate("/sign-in");
+
+      setTimeout(() => {
+        navigate("/sign-in");
+
+      }, 2000);
 
     } catch (err) {
       console.error("Registration error:", err);
@@ -95,12 +99,12 @@ export default function SignUpPage() {
             Create your account and start learning
           </p>
           {successMsg &&(
-            <div className='bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded-lg mb-4 w-full'>
+            <div className='mt-5 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded-lg mb-4 w-full'>
               {successMsg}
             </div>
           )}
           {errorMsg &&(
-            <div className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-lg mb-4 w-full'>
+            <div className='mt-5 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-lg mb-4 w-full'>
               {errorMsg}
             </div>
           )}
