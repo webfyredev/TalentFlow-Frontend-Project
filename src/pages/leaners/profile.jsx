52419c61a3e56b5cd8846eccc515fedec723b637
impphilwebs <imphilwebs@gmail.com>
2026-04-16 11:33:28 +0100

    Working on the profile page

--- src/pages/leaners/profile.jsx
+++ src/pages/leaners/profile.jsx
@@ -115,7 +115,7 @@ export default function LearnerProfile(){
     ]
     return(
         <>
-            <SideBar title="Profile">
+            <SideBar title="Profile" userData={user}>
                 <div className="w-full h-auto py-5 px-7">
                     <div className="w-full p-6 bg-white border-1 border-[#D8D6EF] md:flex md:flex-row  flex flex-col md:space-x-4 mt-5 rounded-lg">
                         <p className="w-16 h-16 rounded-full text-[#1A7A4A] font-semibold bg-[#E8F5EC] flex items-center justify-center">{user.fullName?.split(" ").map(n => n[0]).join("") || "U"}</p>
